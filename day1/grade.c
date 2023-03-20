f8f29820d401428a8ded93ca1c3389b396cc8677
Nagarjuna GS <nagarjunags2014@gmail.com>
2023-03-20 16:01:26 +0530

    a

---
+++ day1/grade.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int m;
+    printf("Enter the marks:");
+    scanf("%d",&m);
+    if(m>85)
+        printf("Distinction\n");
+    else if(m>75)
+        printf("first class\n");
+    else if(m>65)
+        printf("second class\n");
+    else if (m>=35)
+        printf("pass\n");         
+    else    
+        printf("Fail\n");       
+}
