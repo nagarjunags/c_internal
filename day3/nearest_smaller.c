6ce353b2508acb5f8ee6df4648fd7d49140e1851
Nagarjuna GS <nagarjunags2014@gmail.com>
2023-03-23 12:39:39 +0530

    .

--- day3/nearest_smaller.c
+++ day3/nearest_smaller.c
@@ -5,12 +5,7 @@ void main()
     float a[5];
     printf("Enter the 5 values");
     for(int i=0;i<5;i++)
-    {
         scanf("%f",&a[i]);    
-    }
      for(int i=0;i<5;i++)
-    {
         printf("%f\n",floor(a[i]));
-        
-    }
 }
