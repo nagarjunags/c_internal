c118edeffae88235eb8fd545726432195c50fe8f
Nagarjuna GS <nagarjunags2014@gmail.com>
2023-03-23 14:22:53 +0530

    .

--- day3/sum_function.c
+++ day3/sum_function.c
@@ -1,15 +1,29 @@
-//c program to find su using function
+//c program to demonstrate types of functions wrt arguments and return values
 #include<stdio.h>
-void sum(int,int);
+void product();//no arg no return type
+void sum(int,int);//no return but have arg
+diff(int,int);//with arg and with return value
 void main()
 {
     int a,b;
     printf("Enter the number:");
     scanf("%d%d",&a,&b);
+    product();
     sum(a,b);//actual argument
-    sum(7,8);
+    printf("difference is %d\n",diff(a,b));
 }
 void sum(c,d)//formal argument
 {
     printf("Sum=%d\n",c+d);
+}
+int diff(c,d)
+{
+    return c-d;
+}
+void product()
+{
+    int a,b;
+    printf("Enter to multiply:");
+    scanf("%d%d",&a,&b);
+    printf("Product=%d\n",a*b);
 }
