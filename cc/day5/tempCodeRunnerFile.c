7e614962bed0c72284c1d61d3be7fb262ad4cfff
Nagarjun <nagarjun@kali.kali>
2023-04-24 11:08:11 +0530

    .

---
+++ cc/day5/tempCodeRunnerFile.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{
+    int a =600;
+    char *p=&a;
+    
+    printf("%d\n",*p);//this loss of address can be avoided using void pointer and typecasting before the printing
+    printf("%d\n",*(int *)p);//pointer typecasting
+
+
+    return 0;
+    
+}
