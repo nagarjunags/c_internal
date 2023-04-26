a1bf40e27c093a23c700d084f1825e49b2967380
Nagarjun <nagarjun@kali.kali>
2023-04-26 18:17:48 +0530

    .

--- cc/day9/singly_linked_list.c
+++ cc/day9/singly_linked_list.c
@@ -1,3 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+void append(struct Node** head_ref, int new_data);
+void printlist(struct Node* head);
+
+int main() {
+    struct Node* head = NULL;
+    int n, element;
+    
+    // creating a list of n elements
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    printf("Enter the elements: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &element);
+        append(&head, element);
+    }
+    
+    printlist(head);
+    return 0;
+}
+
+void append(struct Node **head_ref, int new_data) {
+    struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
+    new_node->data = new_data;
+    new_node->next = NULL;
+    
+    if (*head_ref == NULL) {
+        *head_ref = new_node;
+        return;
+    }
+    
+    struct Node *current = *head_ref;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+    
+    current->next = new_node;
+}
+
+void printlist(struct Node* head) {
+    while (head != NULL) {
+        printf("%d -> ", head->data);
+        head = head->next;
+    }
+    printf("NULL\n");
+}
+
+
+/*
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -7,6 +63,7 @@ struct Node
     struct Node *next;
 };
 void append(struct Node **head_ref,int new_data);
+void printlist(struct Node **head_ref);
 int main()
 {
     struct Node *head=NULL;
@@ -20,6 +77,7 @@ int main()
         scanf("%d",&element);
         append(&head,element);
     }
+    printlist(&head);
 
     return 0;
 }
@@ -36,8 +94,30 @@ void append(struct Node **head_ref,int new_data)
         while((*previous)->next)
             (*previous)=(*previous)->next;
         struct Node *new_node=(struct Node *)malloc(sizeof(struct Node));
-        (*previous)->next=new_node;
         (new_node)->data=new_data; 
+        (*previous)->next=new_node;
         (new_node)->next=NULL;   
 
 }
+
+
+
+void printlist(struct Node **head_ref)
+{
+    struct Node **previous =head_ref;
+    
+    while((*previous)->next)
+    {
+        printf("%d->",(*previous)->data);
+        (*previous)=(*previous)->next;
+    }
+}
+
+/*void printlist(struct Node* head) {
+    
+    while (head->next != NULL) {
+        printf("%d -> ", head->data);
+        head = head->next;
+    }
+    printf("NULL\n");
+}*/
